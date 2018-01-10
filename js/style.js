e0ff8945b9350335c9e5d6ef56b3792e5e056d7d
winstonfarsoul <matthewjcollier@gmail.com>
2018-01-10 17:11:20 -0500

    ''tentative js implementation''

---
+++ js/style.js
@@ -0,0 +1,43 @@
+// Scroll Function
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      const hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+// End Scroll Function
+
+const panels = document.querySelectorAll('.contact');
+
+function toggleOpen() {
+  this.classList.toggle('open');
+}
+
+function toggleActive(e) {
+  if (e.propertyName.includes('flex')) {
+  this.classList.toggle('open-active');
+}
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
